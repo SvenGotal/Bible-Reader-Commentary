549d1b4cfb15e980bac751d9966ee80073f8dc4a
sven <sven@Synth1N>
2024-01-18 20:43:38 +0100

    rework index.html to accept input for a new user, rework controller to Get and Post data.

--- src/main/java/com/java/crv/controllers/IndexController.java
+++ src/main/java/com/java/crv/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.java.crv.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.java.crv.domain.*;
@@ -12,11 +13,16 @@ public class IndexController {
 	
 	
 	@GetMapping("/")
-	public String setData(Model model) {
+	public String getIndex(Model model) {
 		
 		model.addAttribute("user",new User());
 		
 		return "index";
+	}
+	
+	@PostMapping("/")
+	public String setData(@ModelAttribute("user") User user, Model model) {
+		return "";
 	}
 
 }
