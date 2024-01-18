162fe6f39c4d741c7de2699a9cd2b18b4e4f95f4
sven <sven@Synth1N>
2024-01-18 18:34:33 +0100

    init commit, paste config file, paste files from spring initializr. application.properties file contents: spring.h2.console.enabled=true server.error.path=/error server.error.include-stacktrace=never spring.datasource.url=jdbc:h2:mem:testdb spring.datasource.driverClassName=org.h2.Driver spring.datasource.username=sa spring.datasource.password=password spring.jpa.hibernate.ddl-auto=update

---
+++ src/main/java/BibleReaderCommentary/BibleReaderCommentary/BibleReaderCommentaryApplication.java
@@ -0,0 +1,13 @@
+package BibleReaderCommentary.BibleReaderCommentary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleReaderCommentaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleReaderCommentaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BibleReaderCommentary/BibleReaderCommentary/BibleReaderCommentaryApplicationTests.java
@@ -0,0 +1,13 @@
+package BibleReaderCommentary.BibleReaderCommentary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleReaderCommentaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
