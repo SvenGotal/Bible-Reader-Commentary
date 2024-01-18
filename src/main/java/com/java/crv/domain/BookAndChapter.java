b5e7bff90b14b2bb04d1904681ecc3d3e7f45f23
sven <sven@Synth1N>
2024-01-18 19:35:54 +0100

    Define domain classes

--- src/main/java/com/java/crv/domain/BookAndChapter.java
+++ src/main/java/com/java/crv/domain/BookAndChapter.java
@@ -2,4 +2,33 @@ package com.java.crv.domain;
 
 public class BookAndChapter {
 
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private String Book;
+	private int chapter;
+	
+	public String getBook() {
+		return Book;
+	}
+	public void setBook(String book) {
+		Book = book;
+	}
+	public int getChapter() {
+		return chapter;
+	}
+	public void setChapter(int chapter) {
+		this.chapter = chapter;
+	}
+	@Override
+	public String toString() {
+		return "BookAndChapter [Book=" + Book + ", chapter=" + chapter + "]";
+	}
+	
+	
+	
 }
--- src/main/java/com/java/crv/domain/Commentary.java
+++ src/main/java/com/java/crv/domain/Commentary.java
@@ -2,4 +2,40 @@ package com.java.crv.domain;
 
 public class Commentary {
 
+	private int id;
+	private String text;
+	private User commenter;
+	private BookAndChapter bookAndChapter;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public User getCommenter() {
+		return commenter;
+	}
+	public void setCommenter(User commenter) {
+		this.commenter = commenter;
+	}
+	public BookAndChapter getBookAndChapter() {
+		return bookAndChapter;
+	}
+	public void setBookAndChapter(BookAndChapter bookAndChapter) {
+		this.bookAndChapter = bookAndChapter;
+	}
+	@Override
+	public String toString() {
+		return bookAndChapter.getBook() + " " + bookAndChapter.getChapter() + "\n\n" + text + "\n\n" + commenter;
+	}
+	
+	
+	
 }
