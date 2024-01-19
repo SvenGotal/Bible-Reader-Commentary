cdc55525197dd5787419278804023d96d2ef3f9e
sgotal <sgotal@WMAN9376.ad.local>
2024-01-19 11:51:14 +0100

    add submit form controller

--- src/main/java/com/java/crv/controllers/SubmitFormController.java
+++ src/main/java/com/java/crv/controllers/SubmitFormController.java
@@ -4,15 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.java.crv.domain.User;
 
 @Controller
+@RequestMapping("/submitForm")
 public class SubmitFormController {
 
-	@PostMapping("/submitForm")
+	@GetMapping
+	public String showForm(Model model) {
+		model.addAttribute("user", new User());
+		return "submitform";
+	}
+	
+	@PostMapping
 	public String submitForm(@ModelAttribute("user") @Validated User user, BindingResult bindingResult, Model model) {
 		if(bindingResult.hasErrors())
 			return "submitform";
