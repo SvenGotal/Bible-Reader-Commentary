20dbc0010a661823ec118c71c3f0ca9ce037d30c
sgotal <sgotal@WMAN9376.ad.local>
2024-01-19 11:40:45 +0100

    make some changes

--- src/main/java/com/java/crv/controllers/IndexController.java
+++ src/main/java/com/java/crv/controllers/IndexController.java
@@ -3,11 +3,11 @@ package com.java.crv.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
 import com.java.crv.domain.*;
 import com.java.crv.repository.UserRepository;
 
