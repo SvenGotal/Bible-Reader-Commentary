7308e667d23cf44e9ba08188505ab91c555822a6
sgotal <sgotal@WMAN9376.ad.local>
2024-01-19 09:13:14 +0100

    add user repo as private field

--- src/main/java/com/java/crv/controllers/IndexController.java
+++ src/main/java/com/java/crv/controllers/IndexController.java
@@ -1,15 +1,19 @@
 package com.java.crv.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.java.crv.domain.*;
+import com.java.crv.repository.UserRepository;
 
 @Controller
 public class IndexController {
 	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping("/")
 	public String getIndex(Model model) {
@@ -20,8 +24,10 @@ public class IndexController {
 	}
 	
 	@PostMapping("/")
-	public String setData(@ModelAttribute("user") User user, Model model) {
-		model.addAttribute("message","Data succesfully submitted!");
+	public String setData(@ModelAttribute("user") User user) {
+		
+		userRepository.save(user);
+		
 		return "redirect:/";
 	}
 
