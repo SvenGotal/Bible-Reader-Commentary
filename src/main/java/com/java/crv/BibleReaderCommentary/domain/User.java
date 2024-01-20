232b66fc651d321b9d70d1b6c15ced5090a707e4
sven <sven@Synth1N>
2024-01-20 20:03:01 +0100

    Defining User model as Entity was the key to success. Now repository can be created as a bean. Also added th:field in submitform so the thymeleaf can recognise it as username field.

--- src/main/java/com/java/crv/BibleReaderCommentary/domain/User.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/domain/User.java
@@ -1,12 +1,17 @@
 package com.java.crv.BibleReaderCommentary.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name= "`user`")
 public class User {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) /* DB sama brine o generiranju ID-eva (AUTO) property */
 	private Long id;
 	private String password;	
 	private String username;
