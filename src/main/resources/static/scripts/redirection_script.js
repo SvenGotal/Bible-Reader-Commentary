ae5266ae8fc855cf4e2ca1a4f2edd845c09feb64
sven <sven@Synth1N>
2024-01-23 17:41:37 +0100

    Implement button to submitComment and test redirection

--- src/main/resources/static/scripts/redirection_script.js
+++ src/main/resources/static/scripts/redirection_script.js
@@ -2,10 +2,19 @@ function redirectToUserForm() {
     window.location.href = "/submitForm";
 }
 
+function redirectToCommentForm() {
+	window.location.href = "/submitComment";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     var redirectToUserFormButton = document.getElementById("redirectToUserForm");
+    var redirectToCommentFormButton = document.getElementById("redirectToCommentForm");
     
     if (redirectToUserFormButton) {
         redirectToUserFormButton.addEventListener("click", redirectToUserForm);
     }
+    
+    if (redirectToCommentFormButton){
+		redirectToCommentFormButton.addEventListener("click", redirectToCommentForm);
+	}
 });
