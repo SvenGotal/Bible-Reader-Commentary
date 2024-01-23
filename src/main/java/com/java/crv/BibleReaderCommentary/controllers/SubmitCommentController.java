b3b1d02a9b18eaae39e5964bc81e9a0f9ca3557a
sgotal <sgotal@cxzg-ranzirnikolodvor.acc.t-com.hr>
2024-01-23 09:57:02 +0100

    commit working version. Comments are now working and are being saved successfully. Milestone.

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/SubmitCommentController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/SubmitCommentController.java
@@ -2,14 +2,24 @@ package com.java.crv.BibleReaderCommentary.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.java.crv.BibleReaderCommentary.domain.Commentary;
+import com.java.crv.BibleReaderCommentary.repositories.CommentaryRepository;
 
 @Controller
 @RequestMapping("/submitComment")
 public class SubmitCommentController {
+	
+	private CommentaryRepository commentaryRepository;
+	
+	public SubmitCommentController(CommentaryRepository commentaryRepository) {
+		this.commentaryRepository = commentaryRepository;
+	}
 	
 	@GetMapping
 	public String getCommentForm(Model model) {
@@ -17,6 +27,20 @@ public class SubmitCommentController {
 		return "forms/submitcomment";
 	}
 	
+	@PostMapping
+	public String addComment(@ModelAttribute("comment") Commentary comment, BindingResult validation, Model model) {
+		
+		if(validation.hasErrors()) {
+			model.addAttribute("errormsg", "Binding unsucessful!");
+			System.out.println("!!!ERROR!!!");
+			return "forms/submitComment";
+		}
+		
+		commentaryRepository.save(comment);
+		System.out.println(comment.toString());
+		
+		return "redirect:/";
+	}
 	
 	
 }
