f871d070667943be2a06a81f9e9b71a358e78c10
sven <sven@Synth1N>
2024-01-28 10:57:12 +0100

    COMPLETE todo
    //todo connect User with Commentary in their relation and in persistence

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/SubmitCommentController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/SubmitCommentController.java
@@ -1,5 +1,7 @@
 package com.java.crv.BibleReaderCommentary.controllers;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,17 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.java.crv.BibleReaderCommentary.domain.Commentary;
+import com.java.crv.BibleReaderCommentary.domain.User;
 import com.java.crv.BibleReaderCommentary.repositories.CommentaryRepository;
+import com.java.crv.BibleReaderCommentary.repositories.UserRepository;
 
 @Controller
 @RequestMapping("/submitComment")
 public class SubmitCommentController {
 	
 	private CommentaryRepository commentaryRepository;
+	private UserRepository userRepository;
 	
-	public SubmitCommentController(CommentaryRepository commentaryRepository) {
+	public SubmitCommentController(CommentaryRepository commentaryRepository, UserRepository userRepository) {
 		this.commentaryRepository = commentaryRepository;
+		this.userRepository = userRepository;
 	}
+	
 	
 	@GetMapping
 	public String getCommentForm(Model model) {
@@ -29,12 +36,18 @@ public class SubmitCommentController {
 	}
 	
 	@PostMapping
-	public String addComment(@ModelAttribute("comment") Commentary comment, BindingResult validation, RedirectAttributes redirectAttributes) {		
+	public String addComment(@ModelAttribute("comment") Commentary comment, BindingResult validation, RedirectAttributes redirectAttributes, Principal princ) {		
 		
 		if(validation.hasErrors()) {
 			redirectAttributes.addFlashAttribute("binding", "Failed to insert Data!");		
 			return "forms/submitComment";
 		}
+		System.out.println("Looking for user...");
+		comment.setUser(userRepository.findByUsername(princ.getName()));
+		if(comment.getUser()==null)
+			System.out.println("User not found!");
+		
+		System.out.println("User: " + comment.getUser().getUsername() + " found!");
 				
 		commentaryRepository.save(comment);		
 		redirectAttributes.addFlashAttribute("binding", "Data succesfully stored!");
