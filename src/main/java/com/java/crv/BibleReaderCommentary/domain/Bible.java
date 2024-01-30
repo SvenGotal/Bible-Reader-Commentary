ee1965d72e4b6bfc3d72b9ec3244e639c8d064c4
sven <sven@Synth1N>
2024-01-30 17:40:28 +0100

    generate getters and setters for Bible model
    add emptyField for later use?

--- src/main/java/com/java/crv/BibleReaderCommentary/domain/Bible.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/domain/Bible.java
@@ -16,8 +16,37 @@ public class Bible {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String translation;
+	private String emptyField;
 	
 	@OneToMany(mappedBy = "bible", cascade = CascadeType.ALL)
 	private List<Book> books;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTranslation() {
+		return translation;
+	}
+	public void setTranslation(String translation) {
+		this.translation = translation;
+	}
+	public String getEmptyField() {
+		return emptyField;
+	}
+	public void setEmptyField(String emptyField) {
+		this.emptyField = emptyField;
+	}
+	public List<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+	
+	
+	
 
 }
