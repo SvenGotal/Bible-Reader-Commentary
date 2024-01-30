85448ac3e20a3e07a1047694cdc772c24ddf0588
sven <sven@Synth1N>
2024-01-30 17:28:07 +0100

    Remove xml reading, use excel reading instead

--- src/main/java/com/java/crv/BibleReaderCommentary/objects/ExcelReader.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/objects/ExcelReader.java
@@ -5,6 +5,9 @@ import java.io.FileInputStream;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.java.crv.BibleReaderCommentary.domain.Book;
+import com.java.crv.BibleReaderCommentary.domain.Verse;
+
 public class ExcelReader {
 
 	private String filename;
@@ -15,19 +18,14 @@ public class ExcelReader {
 		this.filename = filename;			
 	}
 	
-	public void read() {
+	public void read(Book book, Verse verse) {
 		
 		try(FileInputStream inputStream = new FileInputStream(filename)){
 			Workbook workbook = new XSSFWorkbook(filename);
 			Sheet sheet = workbook.getSheetAt(0);
 			
 			for(Row row : sheet) {
-				
-				;
-				for(Cell cell : row) {
-					
-				}
-				
+								
 				Cell book = row.getCell(0);
 				Cell chapter = row.getCell(1);
 				Cell verseText = row.getCell(2);
@@ -36,14 +34,10 @@ public class ExcelReader {
 				if(book != null)
 					System.out.println(book.toString() + " " + chapter.toString());
 						
-				String vt = verseText.toString().replaceAll("\\b\\d+(\\.\\d+)?\\b", "");
+				String vt = verseText.toString().replaceAll("\\b\\d+(\\.\\d+)?\\b", "").trim();
 				String vn = verseNumber.toString().replaceAll("\\.0\\b", "");
 				System.out.println(vn + " " + vt);
-//				for(Cell cell : row) {
-//					System.out.print(cell.toString());
-//					System.out.print(" ");
-//					
-//				}
+
 				
 			}
 			
