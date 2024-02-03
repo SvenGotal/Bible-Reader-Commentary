b8d350cdaaaaa487d990cb25122317fd28d16319
sven <sven@Synth1N>
2024-02-03 17:52:26 +0100

    Add line - of no consequence

--- src/main/java/com/java/crv/BibleReaderCommentary/domain/Chapter.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/domain/Chapter.java
@@ -23,9 +23,6 @@ public class Chapter {
 	private Book book;
 	@OneToMany(mappedBy = "chapter", cascade = CascadeType.ALL)
 	private List<Verse> verses;
-	@OneToMany(mappedBy ="chapter", cascade = CascadeType.ALL)
-	private List<Commentary> comments;
-	
 	
 	public Long getId() {
 		return id;
@@ -51,14 +48,6 @@ public class Chapter {
 	public void setVerses(List<Verse> verses) {
 		this.verses = verses;
 	}
-	public List<Commentary> getComments() {
-		return comments;
-	}
-	public void setComments(List<Commentary> comments) {
-		this.comments = comments;
-	}
-	
-	
 	
 	
 }
