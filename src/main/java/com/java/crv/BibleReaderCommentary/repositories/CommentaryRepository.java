912da476430d03fe26283747b1b1cf09628466f6
sven <sven@Synth1N>
2024-02-09 20:29:18 +0100

    Add method for searching comments by their 'published' property

--- src/main/java/com/java/crv/BibleReaderCommentary/repositories/CommentaryRepository.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/repositories/CommentaryRepository.java
@@ -1,5 +1,7 @@
 package com.java.crv.BibleReaderCommentary.repositories;
 
+import java.util.List;
+
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,5 @@ import com.java.crv.BibleReaderCommentary.domain.Commentary;
 @Repository
 @Table
 public interface CommentaryRepository extends CrudRepository<Commentary, Long> {
-
+	List<Commentary> findCommentaryByPublished();
 }
