b822a3ec8997315378546b77d7abb4e4efa52f5d
sven <sven@Synth1N>
2024-02-10 22:32:16 +0100

    Add throwing exceptions and log console in ajax_script.js

--- src/main/resources/static/scripts/ajax_script.js
+++ src/main/resources/static/scripts/ajax_script.js
@@ -36,6 +36,7 @@
     function updateIndexChapters() {
         var selectedBookId = document.getElementById('bookSelection').value;
 
+		console.log("Performing fetch...")
         // Perform an Ajax request to fetch chapters for the selected book
         fetch('/fetchChapters?bookId=' + selectedBookId)
             .then(response => response.json())
@@ -67,3 +68,54 @@
                 console.error('Error fetching chapters:', error);
             });
     }
+    
+    function updateIndexVerses(){
+		
+		/* Find tag element values by Id 
+		Book: Postanak
+		Chapter: 1
+		*/ 
+		
+		console.log("Searching for tag elements and their values...")
+		
+		var selectedBookId = document.getElementById('bookSelection').value;
+		var selectedChapterNumber = document.getElementById('chapterSelection').value;
+		
+		console.log("Found tag elements and their values...")
+		
+		/* Define parameters to send to controller - ApiController */
+		var params = new URLSearchParams();
+		params.append('bookId', selectedBookId);
+		params.append('chapterNumber', selectedChapterNumber);
+		
+		console.log("fetching...");
+		
+		fetch('/fetchVerses?' + params.toString())
+		.then(response => {
+			if(!response.ok){
+				throw new Error("Network response not sending json...");
+			}
+			console.log("Printing parameters: " + params.toString());
+			console.log("Response is ok...");
+			return response.json();
+		})
+		.then(data => {
+			
+			console.log("inside fetch sequence...");
+			
+			var verses = document.getElementById('bible_text');
+			verses.innerHTML = '';
+			
+			console.log("Filling in data...");
+			
+			data.forEach(verse => {
+				var paragraph = document.createElement('p');
+				paragraph.textContent = verse.number + ' ' + verse.text;
+				verses.append(paragraph);
+			});
+			
+		}).catch(error => {
+			console.error('Error fetching verses:', error);
+			});
+		
+	}
