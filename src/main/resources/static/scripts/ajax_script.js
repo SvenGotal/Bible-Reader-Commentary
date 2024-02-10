356f7794b606ae72c034c1fb5cdbcaebfb9fba5f
sven <sven@Synth1N>
2024-02-10 19:57:34 +0100

    Add handling of chapter fetching from index to ajax_script.js

--- src/main/resources/static/scripts/ajax_script.js
+++ src/main/resources/static/scripts/ajax_script.js
@@ -32,3 +32,38 @@
                 console.error('Error fetching chapters:', error);
             });
     }
+    
+    function updateIndexChapters() {
+        var selectedBookId = document.getElementById('bookSelection').value;
+
+        // Perform an Ajax request to fetch chapters for the selected book
+        fetch('/fetchChapters?bookId=' + selectedBookId)
+            .then(response => response.json())
+            .then(data => {
+                // Populate the 'chapterSelection' dropdown based on the response
+                var chapterSelection = document.getElementById('chapterSelection');
+                chapterSelection.innerHTML = ''; // Clear existing options
+
+                // Add a placeholder option
+                var placeholderOption = document.createElement('option');
+                placeholderOption.value = '';
+                placeholderOption.text = 'Odaberite poglavlje';
+                placeholderOption.disabled = true;
+                placeholderOption.selected = true;
+                chapterSelection.appendChild(placeholderOption);
+
+                // Add options for each chapter
+                data.forEach(chapter => {
+                    var option = document.createElement('option');
+                    option.value = chapter.number;
+                    option.text = chapter.number;
+                    chapterSelection.appendChild(option);
+                });
+
+                // Enable the 'chapterSelection' dropdown
+                chapterSelection.disabled = false;
+            })
+            .catch(error => {
+                console.error('Error fetching chapters:', error);
+            });
+    }
