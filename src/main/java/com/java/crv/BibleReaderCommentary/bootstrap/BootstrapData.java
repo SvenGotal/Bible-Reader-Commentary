4815b023d8d77783cea34d6c8f53fb4259fc2231
sven <sven@Synth1N>
2024-02-11 14:21:01 +0100

    Add public comment at bootstrap

--- src/main/java/com/java/crv/BibleReaderCommentary/bootstrap/BootstrapData.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/bootstrap/BootstrapData.java
@@ -1,15 +1,23 @@
 package com.java.crv.BibleReaderCommentary.bootstrap;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.java.crv.BibleReaderCommentary.domain.Bible;
+import com.java.crv.BibleReaderCommentary.domain.Chapter;
+import com.java.crv.BibleReaderCommentary.domain.Commentary;
 import com.java.crv.BibleReaderCommentary.domain.User;
 import com.java.crv.BibleReaderCommentary.domain.UserRoles;
 import com.java.crv.BibleReaderCommentary.objects.ExcelReader;
 import com.java.crv.BibleReaderCommentary.repositories.BibleRepository;
+import com.java.crv.BibleReaderCommentary.repositories.ChapterRepository;
+import com.java.crv.BibleReaderCommentary.repositories.CommentaryRepository;
 import com.java.crv.BibleReaderCommentary.repositories.UserRepository;
 
 @Component
@@ -18,10 +26,14 @@ public class BootstrapData implements CommandLineRunner{
 
 	private final UserRepository userRepo;
 	private final BibleRepository bibleRepo;
+	private final ChapterRepository chapterRepo;
+	private final CommentaryRepository commentRepo;
 	
-	public BootstrapData(UserRepository userRepo, BibleRepository bibleRepo) {
+	public BootstrapData(UserRepository userRepo, BibleRepository bibleRepo, ChapterRepository chapterRepo, CommentaryRepository commentRepo) {
 		this.userRepo = userRepo;
 		this.bibleRepo = bibleRepo;
+		this.chapterRepo = chapterRepo;
+		this.commentRepo = commentRepo;
 	}
 	
 	@Override
@@ -44,12 +56,15 @@ public class BootstrapData implements CommandLineRunner{
 		admin.setUsername("admin");
 		admin.setPassword(encoder.encode("admin"));
 		admin.setRole(UserRoles.ADMIN);
+		admin.setComments(new ArrayList<Commentary>());
 		
 		User simple = new User();
 		simple.setId(109L);
 		simple.setUsername("simpleuser");
 		simple.setPassword(encoder.encode("password"));
 		simple.setRole(UserRoles.USER);
+		
+		
 		
 		System.out.println("Saving data....");		
 		userRepo.save(owner);
@@ -62,6 +77,26 @@ public class BootstrapData implements CommandLineRunner{
 		ExcelReader reader = new ExcelReader("/home/sven/test.xlsx");
 		Bible bible = reader.loadBible("WordProject");
 		bibleRepo.save(bible);
+		
+		
+		Commentary welcome = new Commentary();
+		welcome.setId(1L);
+		Optional<Chapter> chpt = chapterRepo.findById(1L);
+		Chapter metaChapter = new Chapter();
+		metaChapter.setBook(chpt.get().getBook());
+		metaChapter.setId(chpt.get().getId());
+		metaChapter.setNumber(chpt.get().getNumber());
+		metaChapter.setVerses(chpt.get().getVerses());
+		welcome.setChapter(metaChapter);
+		welcome.setUser(admin);
+		welcome.setPublished(true);
+		welcome.setText("Dobrodošli! Molimo Vas da se pridržavate pravila o korištenju ove web stranice.....");
+		welcome.setSubject("Pravila zajednice");
+		
+		commentRepo.save(welcome);
+		
+		admin.getComments().add(welcome);
+		
 		
 	}
 }
