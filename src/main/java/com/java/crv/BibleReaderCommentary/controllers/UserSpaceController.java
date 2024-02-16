b40b1dd900db11e0a1dda5d5d33ed77d612ecb37
sven <sven@Synth1N>
2024-02-16 21:41:01 +0100

    add Post method in userspace controller

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/UserSpaceController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/UserSpaceController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.java.crv.BibleReaderCommentary.domain.Commentary;
 import com.java.crv.BibleReaderCommentary.domain.User;
@@ -37,5 +39,10 @@ public class UserSpaceController {
 	}
 	
 	//todo create PostMapping method, also fetch Commentary Id from displayed comments.
-	
+	@PostMapping("/private/myComments")
+	public String editMyComment(@RequestParam Long commentId, @RequestParam String editedComment) {
+		
+		
+		return "redirect:/";
+	}
 }
