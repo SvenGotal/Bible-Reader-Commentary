4b3d6f0b817ccb8e080389201b7532ff93a77325
root <root@Synth4>
2025-04-17 19:25:56 +0200

    Add user validation and user role to the user space
    User can now be validated and the hamburger menu will work and display
    options accordingly. Same as on the index page.

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/UserSpaceController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/UserSpaceController.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.java.crv.BibleReaderCommentary.domain.Commentary;
 import com.java.crv.BibleReaderCommentary.domain.User;
+import com.java.crv.BibleReaderCommentary.domain.UserRoles;
 import com.java.crv.BibleReaderCommentary.repositories.CommentaryRepository;
 import com.java.crv.BibleReaderCommentary.repositories.UserRepository;
 
@@ -32,16 +33,28 @@ public class UserSpaceController {
 			Principal princ,
 			RedirectAttributes redirectAttributes) {
 		
+		/*Retireve the user name from the Principal and retrieve User by username in repo*/
 		String username = princ.getName();
 		User loggedUser = userRepository.findByUsername(username);
 		
+		/*Fill the ArrayList with comments, empty if there are no comments found*/
 		ArrayList<Commentary> comments = (ArrayList<Commentary>) commentaryRepository.findAllByUserId(loggedUser.getId());
 		
-		model.addAttribute("comments", comments);
-		model.addAttribute("username", loggedUser.getUsername());
+		/*User authentication for options visibility in the hamburger menu*/
+		UserRoles currentUserRole = loggedUser.getRole();
+		Boolean userValidated = loggedUser == null ? false : true;
+		
+		/*Send all the required data to the mycomments View*/
+		model.addAttribute("userRole", currentUserRole.name()); /*Send user role e.g. ADMIN, USER etc...*/
+		model.addAttribute("userValidated", userValidated); 	/*Send whether user is validated*/
 		if(comments.isEmpty()) {
 			model.addAttribute("noCommentsFound", "Trenutno nemate ni jedan komentar!");
 		}
+		/*In the event that user's comments were found send comments data*/
+		else {
+			model.addAttribute("comments", comments);
+		}
+		model.addAttribute("username", loggedUser.getUsername()); /*Send username*/
 		
 		return "forms/mycomments";
 	}
