52e495ac20663e8937649d737e8255ccc802eddf
root <root@Synth4>
2025-04-19 15:26:16 +0200

    Add custom login page
    Refactor comments in the LoginController, add style to the login form.

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/LoginController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/LoginController.java
@@ -2,9 +2,7 @@ package com.java.crv.BibleReaderCommentary.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**This controller is currently unused since Spring Security manages login
- * and logout processes. This is a stub for future custom made login form.
- * */
+/**This is a custom controller for custom login page for the app.*/
 @Controller
 public class LoginController {
 
