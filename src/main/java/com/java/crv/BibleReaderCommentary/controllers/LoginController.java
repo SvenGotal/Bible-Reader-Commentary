2c4c97e41167d518e572300e16b002bbfbec7c0a
root <root@Synth4>
2025-04-19 08:27:12 +0200

    Add documentation and notification
    These controllers are a stub for future custom made login and logout
    Views. Currently the login and logout process is handled by Spring
    Security.

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/LoginController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/LoginController.java
@@ -3,6 +3,9 @@ package com.java.crv.BibleReaderCommentary.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**This controller is currently unused since Spring Security manages login
+ * and logout processes. This is a stub for future custom made login form.
+ * */
 @Controller
 @RequestMapping("/login")
 public class LoginController {
--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/LogoutController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/LogoutController.java
@@ -2,6 +2,9 @@ package com.java.crv.BibleReaderCommentary.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**This controller is currently unused since Spring Security manages login
+ * and logout processes. This is a stub for future custom made logout form.
+ * */
 @Controller
 @RequestMapping("/logout")
 public class LogoutController {
