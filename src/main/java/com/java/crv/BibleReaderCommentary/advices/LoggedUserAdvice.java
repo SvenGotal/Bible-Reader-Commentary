aa342e3508eb1accb042659cb4aacc7a8765c734
root <root@Synth4>
2025-05-20 20:26:57 +0200

    Add logo and user icon fragments
    Apply new fragments to all templates.

---
+++ src/main/java/com/java/crv/BibleReaderCommentary/advices/LoggedUserAdvice.java
@@ -0,0 +1,40 @@
+package com.java.crv.BibleReaderCommentary.advices;
+
+import java.security.Principal;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.java.crv.BibleReaderCommentary.domain.User;
+import com.java.crv.BibleReaderCommentary.repositories.UserRepository;
+
+@ControllerAdvice
+public class LoggedUserAdvice {
+	
+	private UserRepository userRepository;
+	
+	public LoggedUserAdvice(UserRepository userRepository) {
+		this.userRepository = userRepository; 
+	}
+
+	@ModelAttribute
+	public void displayLoggedUser(Model model, Principal principal) {
+		
+		try {
+			if(principal != null) {
+				User currentlyLoggedUser = userRepository.findByUsername(principal.getName());
+				model.addAttribute("currentlyLoggedUser", currentlyLoggedUser);
+			}
+			
+		}
+		catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
