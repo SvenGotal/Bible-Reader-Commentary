5640d6eeddfbd77f3791339ef6ba824120f7fa5a
root <root@Synth4>
2025-07-12 16:19:39 +0200

    Make sure the password column has enough space to contain hashed password

--- src/main/java/com/java/crv/BibleReaderCommentary/domain/User.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/domain/User.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO) /* DB sama brine o generiranju ID-eva (AUTO) property */
 	private Long id;
+	@Column(length = 100)
 	private String password;	
 	private String username;
 	private String email;
