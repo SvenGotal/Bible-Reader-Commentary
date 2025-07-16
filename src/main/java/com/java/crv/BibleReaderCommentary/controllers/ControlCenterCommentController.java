d0194b4b6a27b167e1bcafdff623019f8ff402e1
root <root@Synth4>
2025-07-16 18:47:29 +0200

    Build comment control page

---
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/ControlCenterCommentController.java
@@ -0,0 +1,62 @@
+package com.java.crv.BibleReaderCommentary.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.java.crv.BibleReaderCommentary.domain.Commentary;
+import com.java.crv.BibleReaderCommentary.repositories.CommentaryRepository;
+import com.java.crv.BibleReaderCommentary.repositories.UserRepository;
+
+@Controller
+public class ControlCenterCommentController {
+	private UserRepository userRepository;
+	private CommentaryRepository commentaryRepository;
+
+	
+	public ControlCenterCommentController(UserRepository userRepository, CommentaryRepository commentaryRepository) {
+		this.userRepository = userRepository;
+		this.commentaryRepository = commentaryRepository;
+	}
+	
+	@GetMapping("/admin/commentcontrol")
+	public String getControlCenterCommentsForm(Model model) {
+		
+		try {
+			ArrayList<Commentary> allCommentsInADatabaseList = new ArrayList<Commentary>();
+			allCommentsInADatabaseList = (ArrayList<Commentary>) commentaryRepository.findAll();
+			
+			model.addAttribute("allCommentsInADatabaseList", allCommentsInADatabaseList);
+		}
+		catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+		
+		return "forms/controlcenter_commentspage";
+		
+	}
+	
+	@PostMapping("/admin/deleteCommentFromDatabase")
+	public String deleteCommentFromDatabase(@RequestParam Long commentId, Model model) {
+		
+		String errorDeletingCommentMessage = "Error encountered while deleting commentary";
+		
+		try {
+			
+			commentaryRepository.deleteById(commentId);
+			
+		}
+		catch (NullPointerException e) {
+			e.printStackTrace();
+			model.addAttribute("errorDeletingCommentMessage", errorDeletingCommentMessage);
+		}
+		
+		
+		return "redirect:/admin/commentcontrol";
+		
+	}
+}
--- src/main/resources/static/scripts/control_center_script.js
+++ src/main/resources/static/scripts/control_center_script.js
@@ -6,7 +6,7 @@
 	
 	const deleteUserForm = document.getElementById("deleteUserForm" + deleteUserButton.dataset.userId);		
 	
-	var prompt = window.confirm("Jeste li sigurni da želite korisnika? Biti će obrisani i svi komentari koje je korisnik napisao!");
+	var prompt = window.confirm("Jeste li sigurni da želite izbrisati korisnika? Biti će obrisani i svi komentari koje je korisnik napisao!");
 		
 		if(prompt){
 			deleteUserForm.submit();
@@ -14,4 +14,18 @@
 		else{
 			console.log("cancelled user deletion...");
 		}
+}
+
+function deleteComment(deleteCommentButton){
+	const deleteUserForm = document.getElementById("deleteCommentForm" + deleteCommentButton.dataset.userId);		
+	
+	var prompt = window.confirm("Jeste li sigurni da želite izbrisati komentar?");
+		
+		if(prompt){
+			deleteUserForm.submit();
+		}
+		else{
+			console.log("cancelled comment deletion...");
+		}
+	
 }
