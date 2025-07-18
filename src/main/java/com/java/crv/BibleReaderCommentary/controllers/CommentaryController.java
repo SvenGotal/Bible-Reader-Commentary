e068c6fcc5657c34387a189536da6a532c0bc730
root <root@Synth4>
2025-07-18 19:54:54 +0200

    Remove unused imports

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/CommentaryController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/CommentaryController.java
@@ -1,7 +1,5 @@
 package com.java.crv.BibleReaderCommentary.controllers;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.java.crv.BibleReaderCommentary.domain.Commentary;
 import com.java.crv.BibleReaderCommentary.repositories.CommentaryRepository;
-import com.java.crv.BibleReaderCommentary.repositories.UserRepository;
 
 @Controller
 public class CommentaryController {
-	private UserRepository userRepository;
 	private CommentaryRepository commentaryRepository;
 
 	
-	public CommentaryController(UserRepository userRepository, CommentaryRepository commentaryRepository) {
-		this.userRepository = userRepository;
+	public CommentaryController(CommentaryRepository commentaryRepository) {
 		this.commentaryRepository = commentaryRepository;
 	}
 	
