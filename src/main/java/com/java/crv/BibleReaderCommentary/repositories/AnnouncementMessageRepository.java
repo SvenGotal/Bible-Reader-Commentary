126776e6d4d6dec02d880d8845d9d00d700e2695
root <root@Synth4>
2025-07-26 19:31:29 +0200

    Add new method to find by boolean active- AnnouncementMessageRepository

--- src/main/java/com/java/crv/BibleReaderCommentary/repositories/AnnouncementMessageRepository.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/repositories/AnnouncementMessageRepository.java
@@ -1,10 +1,12 @@
 package com.java.crv.BibleReaderCommentary.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.java.crv.BibleReaderCommentary.domain.AnnouncementMessage;
 
 @Repository
 public interface AnnouncementMessageRepository extends CrudRepository<AnnouncementMessage, Long>{
-
+	public List<AnnouncementMessage> findAllByAnnouncementIsActive(Boolean announcementIsActive);
 }
