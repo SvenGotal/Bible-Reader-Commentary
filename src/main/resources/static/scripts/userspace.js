9536b433cf88b0ac2eb844c54102856044f64a69
root <root@Synth4>
2025-07-31 18:07:25 +0200

    Remove dead and deprecated code

--- src/main/resources/static/scripts/userspace.js
+++ src/main/resources/static/scripts/userspace.js
@@ -54,45 +54,6 @@ function deleteComment(deleteCommentButton){
 	
 }
 
-/* DEAD CODE
-function modifyComment(){
-	
-	var selectedRadio = document.querySelector('input[name="commentSelect"]:checked');
-	var commentIdHolderEdit = document.getElementById("commentIdEdit");
-	var commentIdHolderDelete = document.getElementById("commentIdDelete");
-	
-	var textArea = document.getElementById("commentEditingArea");
-	var subjectArea = document.getElementById("commentSubjectEdit");
-	textArea.textContent = '';
-	subjectArea.value = '';
-	
-	if(selectedRadio){
-		var selectedCommentId = selectedRadio.id;
-		commentIdHolderEdit.value = selectedCommentId;
-		commentIdHolderDelete.value = selectedCommentId;
-		
-		var commentEditingDiv = document.getElementById("commentEditingDiv");
-		commentEditingDiv.style.display = "block";
-		commentEditingDiv.scrollIntoView({
-			behavior: 'smooth',
-			block: 'start'
-		});
-		
-		enableInputFields();
-		
-		var selectedComment = selectedRadio.closest('tr');
-		var rowValues = [];
-		selectedComment.querySelectorAll('td').forEach( function (td) {
-			
-			rowValues.push(td.textContent);
-		});
-		
-		textArea.textContent = rowValues[TEXT_SLOT].trim();
-		subjectArea.value = rowValues[SUBJECT_SLOT];
-		commentMakePrivateOrPublic(rowValues[PUBLISHED_SLOT]);
-	}
-}
-*/
 function submitEditedComment(){
 	var formEdit = document.getElementById("formEdit");	
 	var prompt = window.confirm("Jeste li sigurni da želite izmijeniti komentar?");
@@ -108,25 +69,6 @@ function submitEditedComment(){
 		}
 }
 
-function commentDelete(){
-	
-	var commentIdHolder = document.getElementById("commentIdDelete");
-	var submitForm = document.getElementById("formDelete");
-	var selectedRadio = document.querySelector('input[name="commentSelect"]:checked');
-	
-	if(selectedRadio){
-		
-		commentIdHolder.value = selectedRadio.id;
-		var prompt = window.confirm("Jeste li sigurni da želite obrisati komentar?");
-		
-		if(prompt){
-			submitForm.submit();
-		}
-		else{
-			console.log("user cancelled delete submit...");
-		}
-	}
-}
 
 function commentMakePrivateOrPublic(publishedBoolean){
 	
@@ -139,21 +81,6 @@ function commentMakePrivateOrPublic(publishedBoolean){
 	else{
 		publishedCheckbox.checked = false;
 	}
-}
-
-function enableInputFields(){
-	
-	var textArea = document.getElementById("commentEditingArea");
-	var subjectArea = document.getElementById("commentSubjectEdit");
-	
-	textArea.disabled = false;
-	subjectArea.disabled =false;
-	
-	var buttonEdit = document.getElementById("buttonEdit");
-	var buttonDelete = document.getElementById("buttonDelete");
-		
-	buttonEdit.disabled = false;
-	buttonDelete.disabled = false;
 }
 
 
