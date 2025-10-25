f830d120aba1e8aef209bc4af3377528f8fb04a3
root <root@Synth4>
2025-10-25 13:17:35 +0200

    Add interface method that returns List<Book> rather than Iterable

--- src/main/java/com/java/crv/BibleReaderCommentary/repositories/BookRepository.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/repositories/BookRepository.java
@@ -1,10 +1,12 @@
 package com.java.crv.BibleReaderCommentary.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.java.crv.BibleReaderCommentary.domain.Book;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long>{
-	
+	public List<Book> findAll();
 }
