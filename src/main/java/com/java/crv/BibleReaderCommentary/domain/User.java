7fa3f38cef558a1c38d60f88e56ee51b55ef5877
root <root@Synth4>
2025-10-25 19:25:07 +0200

    Add return this to User setters

--- src/main/java/com/java/crv/BibleReaderCommentary/domain/User.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/domain/User.java
@@ -39,39 +39,45 @@ public class User {
 	public UserRoles getRole() {
 		return role;
 	}
-	public void setRole(UserRoles role) {
+	public User setRole(UserRoles role) {
 		this.role = role;
+		return this;
 	}
 	public List<Commentary> getComments() {
 		return comments;
 	}
-	public void setComments(List<Commentary> comments) {
+	public User setComments(List<Commentary> comments) {
 		this.comments = comments;
+		return this;
 	}
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
+	public User setPassword(String password) {
 		this.password = password;
+		return this;
 	}
 	
 	public Long getId() {
 		return id;
 	}
-	public void setId(Long id) {
+	public User setId(Long id) {
 		this.id = id;
+		return this;
 	}
 	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username) {
+	public User setUsername(String username) {
 		this.username = username;
+		return this;
 	}
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
+	public User setEmail(String email) {
 		this.email = email;
+		return this;
 	}
 	
 	@Override
