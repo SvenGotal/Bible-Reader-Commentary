37a7c09a04b14e0b3ba34422f8166228e7c9eb7e
root <root@Synth4>
2025-10-29 19:45:43 +0100

    Add UserNotFoundException and it's handler

--- src/main/java/com/java/crv/BibleReaderCommentary/advices/CustomExceptionHandlerAdvice.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/advices/CustomExceptionHandlerAdvice.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.java.crv.BibleReaderCommentary.exceptions.BookNotFoundException;
+import com.java.crv.BibleReaderCommentary.exceptions.UserNotFoundException;
 
 @ControllerAdvice
 public class CustomExceptionHandlerAdvice {
 
 	@ExceptionHandler(BookNotFoundException.class)
 	public void handleBookNotFound(BookNotFoundException e, Model model){
+		model.addAttribute("ErrorMsg", e.getMessage());
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public void handleUserNotFound(UserNotFoundException e, Model model){
 		model.addAttribute("ErrorMsg", e.getMessage());
 	}
 }
---
+++ src/main/java/com/java/crv/BibleReaderCommentary/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.java.crv.BibleReaderCommentary.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
