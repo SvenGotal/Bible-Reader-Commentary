fc61ada3882a26dfbeb5f62bdb4bd4457c90b853
root <root@Synth4>
2025-10-29 20:15:14 +0100

    Add javadoc for UserService

--- src/main/java/com/java/crv/BibleReaderCommentary/services/UserService.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/services/UserService.java
@@ -10,6 +10,7 @@ import com.java.crv.BibleReaderCommentary.domain.User;
 import com.java.crv.BibleReaderCommentary.exceptions.UserNotFoundException;
 import com.java.crv.BibleReaderCommentary.repositories.UserRepository;
 
+@SuppressWarnings("unused")
 @Service
 public class UserService {
 	
@@ -19,16 +20,26 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 	
+	/**
+	 * Get all users from the database.
+	 * @return List<User> with all users.
+	 * */
 	public List<User> getAllUsers(){
-		
 		return StreamSupport.stream(userRepository.findAll().spliterator(), false).toList(); 
-		
 	}
 	
+	/**
+	 * Get only one User from the database, by the username.
+	 * @return User
+	 * */
 	public User getUserByUsername(String username) {
 		return userRepository.findByUsername(username);
 	}
 	
+	/**
+	 * Get only one User from the database, by the user's id.
+	 * @return User
+	 * */
 	public User getUserById(Long id) {
 		
 		return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found: " + id));
