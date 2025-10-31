9cf4009a96a1471c202adb67d731558621b8d683
root <root@Synth4>
2025-10-31 19:55:02 +0100

    Handler redirects to error view

--- src/main/java/com/java/crv/BibleReaderCommentary/advices/CustomExceptionHandlerAdvice.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/advices/CustomExceptionHandlerAdvice.java
@@ -13,23 +13,27 @@ import com.java.crv.BibleReaderCommentary.exceptions.UserNotFoundException;
 public class CustomExceptionHandlerAdvice {
 
 	@ExceptionHandler(BookNotFoundException.class)
-	public void handleBookNotFound(BookNotFoundException e, Model model){
+	public String handleBookNotFound(BookNotFoundException e, Model model){
 		model.addAttribute("ErrorMsg", e.getMessage());
+		return "errors/errorView";
 	}
 	
 	@ExceptionHandler(UserNotFoundException.class)
-	public void handleUserNotFound(UserNotFoundException e, Model model){
+	public String handleUserNotFound(UserNotFoundException e, Model model){
 		model.addAttribute("ErrorMsg", e.getMessage());
+		return "errors/errorView";
 	}
 	
 	@ExceptionHandler(AnnouncementMessageNotFoundException.class)
-	public void handleAnnouncementMessageNotFound(AnnouncementMessageNotFoundException e, Model model) {
+	public String handleAnnouncementMessageNotFound(AnnouncementMessageNotFoundException e, Model model) {
 		model.addAttribute("ErrorMsg", e.getMessage());
+		return "errors/errorView";
 	}
 	
 	@ExceptionHandler(CommentaryNotFoundException.class)
-	public void handleCommentaryNotFound(CommentaryNotFoundException e, Model model) {
+	public String handleCommentaryNotFound(CommentaryNotFoundException e, Model model) {
 		model.addAttribute("ErrorMsg", e.getMessage());
+		return "errors/errorView";
 	}
 	
 }
