57923d7aab16e44e7142bbb870d115b9096f7417
root <root@Synth4>
2025-11-13 16:37:30 +0100

    commit

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/ApiController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/ApiController.java
@@ -1,13 +1,11 @@
 package com.java.crv.BibleReaderCommentary.controllers;
 
 import org.springframework.stereotype.Controller;
-import com.java.crv.BibleReaderCommentary.repositories.ChapterRepository;
-import com.java.crv.BibleReaderCommentary.repositories.VerseRepository;
 
 @Controller
 public class ApiController {
 		
-	public ApiController (ChapterRepository chapterRepository, VerseRepository verseRepository) {
+	public ApiController () {
 
 	}		
 	
