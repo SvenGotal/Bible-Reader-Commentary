baa6a181917e183d7a561e8d9f1ed57a742c051e
root <root@Synth4>
2025-11-13 16:36:00 +0100

    Retire APIController for now. Leave stub for future API use.

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/ApiController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/ApiController.java
@@ -1,37 +1,14 @@
 package com.java.crv.BibleReaderCommentary.controllers;
 
-import java.util.List;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.java.crv.BibleReaderCommentary.domain.Chapter;
 import com.java.crv.BibleReaderCommentary.repositories.ChapterRepository;
 import com.java.crv.BibleReaderCommentary.repositories.VerseRepository;
 
 @Controller
 public class ApiController {
-	
+		
+	public ApiController (ChapterRepository chapterRepository, VerseRepository verseRepository) {
 
-	private ChapterRepository chapterRepository;
-
-	
-	public ApiController (ChapterRepository chapterRepository, VerseRepository verseRepository) 
-	{
-
-		this.chapterRepository = chapterRepository;
-
-	}	
-	
-	
-				
-	
-	@GetMapping("/public/fetchAllChapters")
-	@ResponseBody
-	public List<Chapter> fetchFilteredComments(@RequestParam Long chapterId){
-		return chapterRepository.findAllById(chapterId);
-	}
-	
+	}		
 	
 }
