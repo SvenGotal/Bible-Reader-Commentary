bb46b9f166090e509629a3a6a2c2b9757585ac39
root <root@Synth4>
2025-11-13 16:02:11 +0100

    Add VerseController

---
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/VerseController.java
@@ -0,0 +1,29 @@
+package com.java.crv.BibleReaderCommentary.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.java.crv.BibleReaderCommentary.domain.Verse;
+import com.java.crv.BibleReaderCommentary.services.VerseService;
+
+@Controller
+public class VerseController {
+
+	private VerseService verseService;
+	
+	public VerseController(VerseService verseService) {
+		this.verseService = verseService;
+	}
+	
+	@GetMapping("/public/fetchVerses")
+	@ResponseBody
+	public List<Verse> fetchIndexVerses(@RequestParam Long chapterNumber){
+		
+		return verseService.getAllChapterVerses(chapterNumber);
+		
+	}
+}
