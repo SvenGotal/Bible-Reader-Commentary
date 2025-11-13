838ab14cf29a4a1ec01e62e5bee4ce4a587d3918
root <root@Synth4>
2025-11-13 15:45:07 +0100

    Add VerseService

---
+++ src/main/java/com/java/crv/BibleReaderCommentary/services/VerseService.java
@@ -0,0 +1,22 @@
+package com.java.crv.BibleReaderCommentary.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.java.crv.BibleReaderCommentary.domain.Verse;
+import com.java.crv.BibleReaderCommentary.repositories.VerseRepository;
+
+@Service
+public class VerseService {
+
+	private VerseRepository verseRepository;
+	
+	public VerseService(VerseRepository verseRepository) {
+		this.verseRepository = verseRepository;
+	}
+	
+	public List<Verse> getAllChapterVerses(Long chapterId){
+		return verseRepository.findByChapterId(chapterId);
+	}
+}
