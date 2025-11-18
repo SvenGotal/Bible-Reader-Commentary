a14482fb356544f05619e4d18ccadf4639e13faf
root <root@Synth4>
2025-11-18 19:50:15 +0100

    commit work

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/FormLoaderController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/FormLoaderController.java
@@ -44,7 +44,7 @@ public class FormLoaderController {
 		model.addAttribute("books", bookService.getAllBookData());
 		model.addAttribute("announcements", serverAnnouncementService.getAllActiveServerAnnouncementMessages());
 		
-		return "homepage";
+		return "index";
 	}
 	
 	/**
