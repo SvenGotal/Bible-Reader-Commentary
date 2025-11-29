116078b252a8aa9112a5dfded444645cc7ac6654
root <root@Synth4>
2025-11-29 20:40:31 +0100

    commit work

--- src/main/resources/static/scripts/BRC_script.js
+++ src/main/resources/static/scripts/BRC_script.js
@@ -5,11 +5,8 @@
  function closeMenu(){
 	
 	try{
-		var hamburgerToggle = document.getElementById("hamburger_toggle");
-		var hamburgerMenu = document.getElementById("hamburger_menu");
-		
+		var hamburgerToggle = document.getElementById("hamburger_toggle");		
 		hamburgerToggle.checked = false;
-		hamburgerMenu.style.height = "0px";
 	}
 	catch(e){
 		
