e054aba749df8eba056a771c3d95a9bd426ce52b
sven <sven@Synth4>
2026-07-29 19:35:02 +0200

    RestController instead of Controller

--- src/main/java/com/java/crv/BibleReaderCommentary/controllers/BookController.java
+++ src/main/java/com/java/crv/BibleReaderCommentary/controllers/BookController.java
@@ -1,16 +1,16 @@
 package com.java.crv.BibleReaderCommentary.controllers;
 
 import java.util.List;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.java.crv.BibleReaderCommentary.domain.Book;
 import com.java.crv.BibleReaderCommentary.domain.User;
 import com.java.crv.BibleReaderCommentary.services.BookService;
 
-@Controller
+@RestController
 public class BookController {
 	
 	private BookService bookService;
@@ -29,7 +29,6 @@ public class BookController {
 	@ResponseBody
 	public List<Book> fetchCommentedBooks(@ModelAttribute("currentlyLoggedUser") User currentlyLoggedUser) {
 				
-		// TODO: when unsigned user searches only commented books, if book contains one private comment, it is shown in the select.
 		return bookService.getBooksThatContainComments(currentlyLoggedUser.getId());
 	}	
 }
