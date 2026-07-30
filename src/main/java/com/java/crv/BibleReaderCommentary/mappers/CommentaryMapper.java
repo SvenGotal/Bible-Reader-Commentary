c6e4c64ef4d52c835516c30fd35a1029bf0b2274
sven <sven@Synth4>
2026-07-30 17:46:30 +0200

    add Mapper for Commentary DTO-entity

---
+++ src/main/java/com/java/crv/BibleReaderCommentary/mappers/CommentaryMapper.java
@@ -0,0 +1,35 @@
+package com.java.crv.BibleReaderCommentary.mappers;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
+
+import com.java.crv.BibleReaderCommentary.domain.Commentary;
+import com.java.crv.BibleReaderCommentary.domainDTO.CommentaryDTO;
+
+public class CommentaryMapper {
+
+	private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+	
+	public CommentaryDTO toDto(Commentary entity) {
+		if(entity == null)
+			return null;
+		
+		CommentaryDTO cdto = new CommentaryDTO();
+		cdto.setId(entity.getId());
+		cdto.setSubject(entity.getSubject());
+		cdto.setPublished(entity.getPublished());
+		cdto.setText(entity.getText());
+		cdto.setDateOfCreation(entity.getDateOfCreation().format(format));
+		cdto.setAuthor(entity.getAuthor());
+		cdto.setUserId(entity.getUser().getId());
+		cdto.setChapterId(entity.getChapter().getId());
+		
+		return cdto;
+	}
+	
+	public Commentary toEntity(CommentaryDTO cdto) {
+		//TODO: stub
+		return null;
+	}
+}
